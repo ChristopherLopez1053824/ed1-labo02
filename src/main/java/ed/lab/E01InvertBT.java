02d8eca41cf9ceb8554b2226ebb47efbf4cc4cd4
ChristopherLopez <conradoclv@gmail.com>
2025-03-20 15:27:17 -0600

    Prueba 1 Invertir Arbol

--- src/main/java/ed/lab/E01InvertBT.java
+++ src/main/java/ed/lab/E01InvertBT.java
@@ -1,7 +1,18 @@
 package ed.lab;
-
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 public class E01InvertBT {
+    //PREORDER
     public TreeNode<Integer> invertTree(TreeNode<Integer> root) {
+        if (root == null)
+            return null;
+        TreeNode<Integer> Aux = root.right;
+        root.right = root.left;
+        root.left = Aux;
+        invertTree(root.left);
+        invertTree(root.right);
         return root;
     }
+
 }
