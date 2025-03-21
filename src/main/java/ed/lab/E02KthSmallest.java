007baab37a414897e36beb72a4c0f53107aeaeb2
ChristopherLopez <conradoclv@gmail.com>
2025-03-21 14:33:20 -0600

    Prueba 1 Laboratorio 2 completado COn todo funcionando

--- src/main/java/ed/lab/E02KthSmallest.java
+++ src/main/java/ed/lab/E02KthSmallest.java
@@ -1,42 +1,27 @@
 package ed.lab;
 
 public class E02KthSmallest {
-
+    int Memory=Integer.MAX_VALUE;
+    int Contador = 0;
     public int kthSmallest(TreeNode<Integer> root, int k) {
-        if(EncontrarK(root,k)==true)
-            return k;
-
-        return Smallest(root, k, Integer.MAX_VALUE);
-    }
-
-    public boolean EncontrarK(TreeNode<Integer> root, int k)
-    {
-        if(root==null)
-            return false;
-
-        if(k==root.value)
-            return true;
-
-        if(k<root.value)
-            return EncontrarK(root.left,k);
-        if(k>root.value)
-            return EncontrarK(root.right,k);
-        return false;
-    }
-
-    public int Smallest(TreeNode<Integer> root, int k, int Memory)
-    {
-        if(root==null)
-            return Memory;
-
-        if(root.value>k && root.value<Memory)
-            Memory =  root.value;
-
-        if(k<root.value)
-            return Smallest(root.left, k, Memory);
-        if(k>root.value)
-            return Smallest(root.right,k, Memory);
-
+        Smallest(root, k);
         return Memory;
+    }
+
+
+    public void Smallest(TreeNode<Integer> root, int k)
+    {
+        if (root == null)
+            return;
+
+        Smallest(root.left, k);
+
+        Contador++;
+        if (Contador == k) {
+            Memory = root.value;
+            return;
+        }
+
+        Smallest(root.right, k);
     }
 }
